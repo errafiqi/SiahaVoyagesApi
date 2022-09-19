2b5f3536a8116cf364c285e4ff30ee4ffbe7279d
errafiqi <errafiqi.zakaria.std@gmail.com>
2022-09-19 16:39:50 +0100

    Migration oubliée again

--- src/SiahaVoyages.EntityFrameworkCore/Migrations/20220919153753_alter_Driver_add_Logo.Designer.cs
+++ src/SiahaVoyages.EntityFrameworkCore/Migrations/20220919153753_alter_Driver_add_Logo.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SiahaVoyages.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,10 @@ using Volo.Abp.EntityFrameworkCore;
 namespace SiahaVoyages.Migrations
 {
     [DbContext(typeof(SiahaVoyagesDbContext))]
-    partial class SiahaVoyagesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220919153753_alter_Driver_add_Logo")]
+    partial class alter_Driver_add_Logo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/SiahaVoyages.EntityFrameworkCore/Migrations/20220919153753_alter_Driver_add_Logo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SiahaVoyages.Migrations
+{
+    public partial class alter_Driver_add_Logo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePicture",
+                table: "App_Driver",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePicture",
+                table: "App_Driver");
+        }
+    }
+}
